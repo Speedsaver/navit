66afd7e011263053a9dc7ec792806d5ff763fa2e
Jookia <contact@jookia.org>
2022-11-20 01:10:20 +0000

    Hard code builtin.c

--- navit/builtin.c
+++ navit/builtin.c
@@ -1,11 +1,9 @@
-${MODULES_PROTOTYPE};
 extern void builtin_init(void); 
 void	module_map_binfile_init(void);
 void	module_graphics_ssd1306_init(void);
 void    module_vehicle_gpsd_init(void);
 void    module_gui_speedsaver_init(void);
 void builtin_init(void) { 
-    ${MODULES_FUNC};
     module_map_binfile_init();
     module_graphics_ssd1306_init();
     module_vehicle_gpsd_init();
